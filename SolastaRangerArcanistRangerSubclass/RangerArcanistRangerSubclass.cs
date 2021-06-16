6d5443bb2a6838111bcdb1ece2ed8ef17d60d40b
ElAntonius <elantonius@gmail.com>
2021-06-16 16:02:34 -0400

    Developed preliminary damage portion of mark power. Will need to rethink, or evaluate how to make the damage apply only to owner of mark.

--- SolastaRangerArcanistRangerSubclass/RangerArcanistRangerSubclass.cs
+++ SolastaRangerArcanistRangerSubclass/RangerArcanistRangerSubclass.cs
@@ -10,18 +10,22 @@ namespace SolastaRangerArcanistRangerSubclass
 {
     class RangerArcanistRangerSubclass
     {
+
+
         const string RangerArcanistRangerSubclassName = "RangerArcanistRangerSubclass";
-        const string RangerArcanistRangerSubclassNameGuid = "5ABD870D-9ABD-4953-A2EC-E2109324FAB9";
-        
+        const string RangerArcanistRangerSubclassGuid = "5ABD870D-9ABD-4953-A2EC-E2109324FAB9";
+
+        public static Guid RA_BASE_GUID = new Guid(RangerArcanistRangerSubclassGuid);
+
         public static void BuildAndAddSubclass()
         {
             var subclassGuiPresentation = new GuiPresentationBuilder(
                     "Subclass/&RangerArcanistRangerSubclassDescription",
                     "Subclass/&RangerArcanistRangerSubclassTitle")
-                    .SetSpriteReference(DatabaseHelper.CharacterSubclassDefinitions.MartialSpellblade.GuiPresentation.SpriteReference)
+                    .SetSpriteReference(DatabaseHelper.CharacterSubclassDefinitions.RoguishShadowCaster.GuiPresentation.SpriteReference)
                     .Build();
 
-            var definition = new CharacterSubclassDefinitionBuilder(RangerArcanistRangerSubclassName, RangerArcanistRangerSubclassNameGuid)
+            var definition = new CharacterSubclassDefinitionBuilder(RangerArcanistRangerSubclassName, RangerArcanistRangerSubclassGuid)
                     .SetGuiPresentation(subclassGuiPresentation)
                     .AddFeatureAtLevel(ArcanistMarkPowerBuilder.ArcanistMarkPower, 3)
                     .AddToDB();
@@ -33,7 +37,6 @@ namespace SolastaRangerArcanistRangerSubclass
     internal class ArcanistMarkPowerBuilder : BaseDefinitionBuilder<FeatureDefinitionPower>
     {
         const string ArcanistMarkPowerName = "ArcanistMarkPower";
-        const string ArcanistMarkPowerNameGuid = "E2C7DBDD-5218-419E-89FE-5457FBCD7A0C";
 
         protected ArcanistMarkPowerBuilder(string name, string guid) : base(DatabaseHelper.FeatureDefinitionPowers.PowerDomainBattleDecisiveStrike, name, guid)
         {
@@ -43,41 +46,47 @@ namespace SolastaRangerArcanistRangerSubclass
             Definition.SetRechargeRate(RuleDefinitions.RechargeRate.LongRest);
             Definition.SetActivationTime(RuleDefinitions.ActivationTime.OnAttackHit);
             Definition.SetUsesDetermination(RuleDefinitions.UsesDetermination.ProficiencyBonus);
+            Definition.SetUniqueInstance(true);
             Definition.SetCostPerUse(1);
             Definition.SetShortTitleOverride("Feature/&ArcanistMarkPowerTitle");
 
-            //Create the Mark effect
+            // Create the Mark effect
             EffectForm markEffect = new EffectForm();
             markEffect.ConditionForm = new ConditionForm();
             markEffect.FormType = EffectForm.EffectFormType.Condition;
             markEffect.ConditionForm.Operation = ConditionForm.ConditionOperation.Add;
             markEffect.ConditionForm.ConditionDefinition = ArcanistMarkConditionBuilder.ArcanistMarkCondition;
 
-            //Add to our new effect
-            EffectDescription newEffectDescription = new EffectDescription();
-            newEffectDescription.Copy(Definition.EffectDescription);
-            newEffectDescription.EffectForms.Clear();
-            newEffectDescription.EffectForms.Add(markEffect);
-            newEffectDescription.HasSavingThrow = false;
-            newEffectDescription.DurationType = RuleDefinitions.DurationType.Minute;
-            newEffectDescription.DurationParameter = 1;
-            //newEffectDescription.
+            // Add the Damage Dice
+            EffectForm markDamageEffect = new EffectForm();
+            markDamageEffect.DamageForm = new DamageForm();
+            markDamageEffect.DamageForm.DiceNumber = 1;
+            markDamageEffect.DamageForm.DieType = RuleDefinitions.DieType.D4;
+            markDamageEffect.DamageForm.DamageType = "DamageForce";
+            markDamageEffect.SavingThrowAffinity = RuleDefinitions.EffectSavingThrowType.None;
 
-            Definition.SetEffectDescription(newEffectDescription);
+            // Create a Description for the Mark Effect
+            EffectDescriptionBuilder markEffectDescriptionBuilder = new EffectDescriptionBuilder();
+            markEffectDescriptionBuilder.SetCreatedByCharacter();
+            markEffectDescriptionBuilder.SetDurationData(RuleDefinitions.DurationType.Minute, 1, RuleDefinitions.TurnOccurenceType.EndOfTurn);
+            markEffectDescriptionBuilder.SetTargetingData(RuleDefinitions.Side.Enemy, RuleDefinitions.RangeType.RangeHit, 120, RuleDefinitions.TargetType.Individuals, 1, 1, ActionDefinitions.ItemSelectionType.Weapon);
+            markEffectDescriptionBuilder.AddEffectForm(markEffect);
+            //markEffectDescriptionBuilder.AddEffectForm(markDamageEffect);
+
+            Definition.SetEffectDescription(markEffectDescriptionBuilder.Build());
         }
 
         public static FeatureDefinitionPower CreateAndAddToDB(string name, string guid)
             => new ArcanistMarkPowerBuilder(name, guid).AddToDB();
 
-        public static FeatureDefinitionPower ArcanistMarkPower = CreateAndAddToDB(ArcanistMarkPowerName, ArcanistMarkPowerNameGuid);
+        public static FeatureDefinitionPower ArcanistMarkPower = CreateAndAddToDB(ArcanistMarkPowerName, GuidHelper.Create(RangerArcanistRangerSubclass.RA_BASE_GUID, ArcanistMarkPowerName).ToString());
     }
 
     internal class ArcanistMarkConditionBuilder : BaseDefinitionBuilder<ConditionDefinition>
     {
         const string ArcanistMarkConditionName = "ArcanistMarkCondition";
-        const string ArcanistMarkConditionNameGuid = "944DE98F-1CFD-4DEA-BE3E-CECDC98B7C35";
 
-        protected ArcanistMarkConditionBuilder(string name, string guid) : base(DatabaseHelper.ConditionDefinitions.ConditionHeraldOfBattle, name, guid)
+        protected ArcanistMarkConditionBuilder(string name, string guid) : base(DatabaseHelper.ConditionDefinitions.ConditionMarkedByHunter, name, guid)
         {
             Definition.GuiPresentation.Title = "Feature/&ArcanistMarkConditionTitle";
             Definition.GuiPresentation.Description = "Feature/&ArcanistMarkConditionDescription";
@@ -86,17 +95,17 @@ namespace SolastaRangerArcanistRangerSubclass
             Definition.Features.Clear();
             Definition.SetDurationType(RuleDefinitions.DurationType.Minute);
             Definition.SetDurationParameter(1);
+            Definition.SetConditionType(RuleDefinitions.ConditionType.Detrimental);
+            Definition.SetAdditionalDamageWhenHit(true);
+            Definition.SetAdditionalDamageType("DamageForce");
+            Definition.SetAdditionalDamageDieType(RuleDefinitions.DieType.D4);
+            Definition.SetAdditionalDamageDieNumber(1);
         }
 
         public static ConditionDefinition CreateAndAddToDB(string name, string guid)
             => new ArcanistMarkConditionBuilder(name, guid).AddToDB();
 
-        public static ConditionDefinition ArcanistMarkCondition = CreateAndAddToDB(ArcanistMarkConditionName, ArcanistMarkConditionNameGuid);
+        public static ConditionDefinition ArcanistMarkCondition = CreateAndAddToDB(ArcanistMarkConditionName, GuidHelper.Create(RangerArcanistRangerSubclass.RA_BASE_GUID, ArcanistMarkConditionName).ToString());
     }
 
-    internal class ArcanistMarkEffectBuilder : BaseDefinitionBuilder<EffectDescription>
-    {
-        const string ArcanistMarkEffectName = "ArcanistMarkEffect";
-        const string ArcanistMarkEffectNameGuid =
-    }
 }
